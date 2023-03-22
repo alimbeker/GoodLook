2b0bc11b5e85a53bf2f6d52dff3d5ccc74337eac
Alimbekerov Amir <200103054@stu.sdu.edu.kz>
2023-03-22 18:13:32 +0600

    add variables and connect with interface classes

---
+++ app/src/main/java/com/example/goodlook/Interface.kt
@@ -0,0 +1,17 @@
+package com.example.goodlook
+
+import android.view.View
+
+interface DateClickListener {
+    fun onDateClick(v: View) {
+
+    }
+}
+
+interface TimeClickListener {
+    fun onTimeClick(v:View) {
+
+
+    }
+}
+
