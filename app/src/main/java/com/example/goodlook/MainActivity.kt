3276abdd41caa50cba234131ce442b9c31c44a19
Alimbekerov Amir <200103054@stu.sdu.edu.kz>
2023-04-16 14:45:08 +0600

    add new bottom design

--- app/src/main/java/com/example/goodlook/MainActivity.kt
+++ app/src/main/java/com/example/goodlook/MainActivity.kt
@@ -2,22 +2,45 @@ package com.example.goodlook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationItemView
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.fragment.app.Fragment
+import com.ismaeldivita.chipnavigation.ChipNavigationBar
+
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var navController: NavController
+
+    private lateinit var chipNavigationBar: ChipNavigationBar
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        chipNavigationBar = findViewById(R.id.bottom_nav_bar)
+        chipNavigationBar.setItemSelected(R.id.favorFragment, true)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.mainContainer) as NavHostFragment
-        navController = navHostFragment.navController
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
-        setupWithNavController(bottomNavigationView, navController)
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, FavorFragment())
+            .commit()
+
+        bottomMenu()
+    }
+
+    private fun bottomMenu() {
+        chipNavigationBar.setOnItemSelectedListener(
+            object : ChipNavigationBar.OnItemSelectedListener {
+                override fun onItemSelected(i: Int) {
+                    var fragment: Fragment? = null
+                    when (i) {
+                        R.id.favorFragment -> fragment = FavorFragment()
+                        R.id.catFrag -> fragment = CatFrag()
+                        R.id.recomFrag -> fragment = RecomFrag()
+                        R.id.korzinFrag -> fragment = KorzinFrag()
+                    }
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, fragment!!)
+                        .commit()
+                }
+            }
+        )
     }
 }
