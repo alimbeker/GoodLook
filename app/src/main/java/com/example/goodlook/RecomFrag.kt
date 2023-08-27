39f3763062c89fae3d0aba7cb4eb9ac1eb4a46f8
Alimbekerov Amir <200103054@stu.sdu.edu.kz>
2023-08-27 22:52:51 +0600

    Add spinner

--- app/src/main/java/com/example/goodlook/RecomFrag.kt
+++ app/src/main/java/com/example/goodlook/RecomFrag.kt
@@ -50,6 +50,19 @@ class RecomFrag : Fragment(),
         val vmFactory = VmFactory(dataSource,application)
         val vm = ViewModelProvider(this,vmFactory).get(FavorFragmentViewModel::class.java)
 
+        // Populate the Spinner with items
+        val spinner: Spinner = binding.spinner
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.spinner_items,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinner.adapter = adapter
+        }
 
         binding.saveCard.setOnClickListener {
             val newCardTask = binding.newCardTask.text.toString()
