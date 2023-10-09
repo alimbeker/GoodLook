02605b91fd402ac6bfad2c3a13b5f2fe0fe14547
Alimbekerov Amir <200103054@stu.sdu.edu.kz>
2023-10-09 15:40:05 +0600

    add vm.onChecked() into ViewHolder

--- app/src/main/java/com/example/goodlook/view/ItemAdapter.kt
+++ app/src/main/java/com/example/goodlook/view/ItemAdapter.kt
@@ -14,12 +14,25 @@ import com.example.goodlook.viewmodel.FavorFragmentViewModel
 
 open class ItemAdapter(private val vm : FavorFragmentViewModel):ListAdapter<CardEntity,ItemAdapter.ViewHolder>(CardDiffCallback()){
 
-    class ViewHolder(private val binding: ListItemBinding):RecyclerView.ViewHolder(binding.root){
-        fun bind(card:CardEntity)=with(binding){
-            listName.text = card.cardName
-            //to print
-            listTime.text = card.getFormattedDeadline()
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder.create(parent)
+    }
 
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val card = getItem(position)
+
+        holder.bind(card,vm)
+    }
+
+    class ViewHolder(private val binding: ListItemBinding):RecyclerView.ViewHolder(binding.root){
+        fun bind(card:CardEntity, vm: FavorFragmentViewModel) {
+            binding.listName.text = card.cardName
+            //to print
+            binding.listTime.text = card.getFormattedDeadline()
+            binding.listImage.setOnClickListener {
+                binding.listImage.setImageResource(R.drawable.checked)
+                vm.onChecked(card)
+            }
         }
         companion object{
             fun create (parent:ViewGroup):ViewHolder{
@@ -27,22 +40,6 @@ open class ItemAdapter(private val vm : FavorFragmentViewModel):ListAdapter<Card
             }
         }
 
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder.create(parent)
-    }
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val card = getItem(position)
-        holder.itemView.findViewById<ImageView>(R.id.listImage).setOnClickListener {
-
-            holder.itemView.findViewById<ImageView>(R.id.listImage).setImageResource(R.drawable.checked)
-
-            vm.onChecked(card)
-
-        }
-        holder.bind(getItem(position))
     }
 }
 class CardDiffCallback: DiffUtil.ItemCallback<CardEntity>() {
