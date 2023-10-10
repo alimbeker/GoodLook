53c06b3a77a5a4a8d2b54840f2f7f658228d1020
Alimbekerov Amir <200103054@stu.sdu.edu.kz>
2023-10-10 19:22:15 +0600

    adding viewmodel in BaseFragment

--- app/src/main/java/com/example/goodlook/BaseFragment.kt
+++ app/src/main/java/com/example/goodlook/BaseFragment.kt
@@ -5,14 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.viewbinding.ViewBinding
+import com.example.goodlook.database.CardDatabase
+import com.example.goodlook.viewmodel.FavorFragmentViewModel
+import com.example.goodlook.viewmodel.VmFactory
 
 typealias Inflate<T> = (LayoutInflater, ViewGroup?, Boolean) -> T
 
 abstract class BaseFragment<VB: ViewBinding>(private val inflate: Inflate<VB>) : Fragment() {
     private var _binding : VB? = null
+    private var _viewModel : FavorFragmentViewModel? = null
 
     val binding get() = _binding ?: throw RuntimeException()
+
+    val viewModel get() = _viewModel ?: throw RuntimeException()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,6 +27,11 @@ abstract class BaseFragment<VB: ViewBinding>(private val inflate: Inflate<VB>) :
         savedInstanceState: Bundle?
     ): View? {
         _binding = inflate(inflater, container, false)
+        val application = requireNotNull(this.activity).application
+        val dataSource = CardDatabase.getInstance(application)!!.cardDao()
+        val vmFactory = VmFactory(dataSource,application)
+        _viewModel = ViewModelProvider(this,vmFactory).get(FavorFragmentViewModel::class.java)
+
 
         return binding.root
     }
