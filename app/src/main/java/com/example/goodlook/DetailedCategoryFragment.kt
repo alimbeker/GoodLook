94a89ecbe074dcbb351daf0b99f28dfcf3093d49
Amir Alimbekerov <200103054@stu.sdu.edu.kz>
2023-11-26 17:37:40 +0600

    create DetailedCategoryFragment

---
+++ app/src/main/java/com/example/goodlook/DetailedCategoryFragment.kt
@@ -0,0 +1,35 @@
+package com.example.goodlook
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.goodlook.database.CardDatabase
+import com.example.goodlook.databinding.FragmentKorzinBinding
+import com.example.goodlook.view.ItemAdapter
+import com.example.goodlook.viewmodel.FavorFragmentViewModel
+import com.example.goodlook.viewmodel.VmFactory
+
+class DetailedCategoryFragment : Fragment() {
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentKorzinBinding.inflate(inflater, container, false)
+
+
+
+
+
+        return binding.root
+
+    }
+
+
+}
