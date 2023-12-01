51825db6426dcff006e2aca0504f5dfc5257cb22
Amir Alimbekerov <200103054@stu.sdu.edu.kz>
2023-12-01 21:40:56 +0600

    DetailedCategoryFragment is inherited from BaseFragment

--- app/src/main/java/com/example/goodlook/DetailedCategoryFragment.kt
+++ app/src/main/java/com/example/goodlook/DetailedCategoryFragment.kt
@@ -8,39 +8,45 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.goodlook.database.CardDatabase
+import com.example.goodlook.databasecategory.CategoryDatabase
 import com.example.goodlook.databinding.FragmentDetailedCategoryBinding
+import com.example.goodlook.databinding.FragmentFavorBinding
 import com.example.goodlook.view.ItemAdapter
+import com.example.goodlook.viewmodel.CategoryViewModel
 import com.example.goodlook.viewmodel.FavorFragmentViewModel
 import com.example.goodlook.viewmodel.VmFactory
 
-class DetailedCategoryFragment : Fragment() {
+class DetailedCategoryFragment : BaseFragment<FragmentDetailedCategoryBinding>(FragmentDetailedCategoryBinding::inflate) {
+
+
+    private lateinit var card_viewModel: FavorFragmentViewModel
+
     private lateinit var itemAdapter: ItemAdapter
 
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val binding = FragmentDetailedCategoryBinding.inflate(inflater, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+
+        // Safe args
         val args = DetailedCategoryFragmentArgs.fromBundle(requireArguments())
         binding.contentTitle.text = args.title
 
-        // Implement viewModel
+
+        // Implement viewmodel
         val application = requireNotNull(this.activity).application
+        val dataSource = CardDatabase.getInstance(application)!!.cardDao()
+
+        card_viewModel = createViewModel(application, dataSource)
 
 
-        // Implement viewModel for FavorFragmentViewModel
-        val cardDataSource = CardDatabase.getInstance(application)!!.cardDao()
-        val cardVmFactory = VmFactory(cardDataSource, application, FavorFragmentViewModel::class.java)
-        val vm = ViewModelProvider(this, cardVmFactory).get(FavorFragmentViewModel::class.java)
 
         //Get list
         val itemRecyclerView = binding.childRecyclerView
-        itemAdapter = ItemAdapter(vm)
+        itemAdapter = ItemAdapter(card_viewModel)
         itemRecyclerView.adapter = itemAdapter
 
-        vm.filteredCards.observeForever { allCards ->
+        card_viewModel.filteredCards.observeForever { allCards ->
             // Filter favorCards based on the category or any other logic
             val filteredFavorCards = allCards.filter { it.cardCategory == args.title }
             itemAdapter.submitList(filteredFavorCards)
@@ -50,16 +56,12 @@ class DetailedCategoryFragment : Fragment() {
             layoutManager = LinearLayoutManager(this.context)
             adapter = itemAdapter
         }
+
+
         //OffsetDecoration
         val offsetDecoration = OffsetDecoration(start = 16, top = 16, end = 15, bottom = 16)
         itemRecyclerView.addItemDecoration(offsetDecoration)
 
-
-
-
-
-
-        return binding.root
 
     }
 
